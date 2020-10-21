bccfd1e1c3ca8bcbb7ca633aaa94ad3493717849
彭道宽 <pengdaokuan@cvte.com>
2020-10-21 10:09:29 +0800

    [feature][what]添加数据模型

---
+++ src/model/font.model.ts
@@ -0,0 +1,12 @@
+/**
+ * @description 字体数据模型
+ */
+import { IFontModel } from '@common/types/model';
+
+export const FontModel: IFontModel = {
+  fontColor: '',
+  fontFamily: '',
+  fontSize: '',
+  lineHeight: '',
+  letterSpacing: ''
+};

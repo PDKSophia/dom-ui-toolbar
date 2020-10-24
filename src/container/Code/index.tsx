afc9d4e024f1019cfd27b8c6b87f4603cd9c1a73
彭道宽 <pengdaokuan@cvte.com>
2020-10-24 11:20:09 +0800

    [feature][what]添加code样式demo代码

--- src/container/Code/index.tsx
+++ src/container/Code/index.tsx
@@ -6,20 +6,43 @@ import styles from './index.less';
 import Title from '@components/Toolbar/Title';
 import Textarea from '@components/Design/Textarea';
 
-class Code extends React.Component {
-  render() {
-    return (
-      <div className={styles.code}>
-        <Title text="代码" />
-        <div className={styles.block}>
-          <Textarea
-            iClassName={styles.custom}
-            value="艾宾斯浩遗忘曲线告诉我们：人类大脑对新事物的遗忘是有规律的。而人们恰恰可以从遗忘曲线中找到记忆规律。科学的周期，将帮助你规律复习，高质量备考。"
-          />
-        </div>
+function Code() {
+  return (
+    <div className={styles.code}>
+      <Title text="代码" />
+      <div className={styles.block}>
+        <p className={styles.property}>
+          <span className={styles.key}>width</span>
+          <span className={styles.line}>: </span>
+          <span className={styles.value}>294px</span>
+          <span className={styles.line}>;</span>
+        </p>
+        <p className={styles.property}>
+          <span className={styles.key}>height</span>
+          <span className={styles.line}>: </span>
+          <span className={styles.value}>96px</span>
+          <span className={styles.line}>;</span>
+        </p>
+        <p className={styles.property}>
+          <span className={styles.key}>font-size</span>
+          <span className={styles.line}>: </span>
+          <span className={styles.value}>12px</span>
+          <span className={styles.line}>;</span>
+        </p>
+        <p className={styles.property}>
+          <span className={styles.key}>font-weight</span>
+          <span className={styles.line}>: </span>0<span className={styles.value}>400</span>
+          <span className={styles.line}>;</span>
+        </p>
+        <p className={styles.property}>
+          <span className={styles.key}>color</span>
+          <span className={styles.line}>: </span>
+          <span className={styles.value}>#000000</span>
+          <span className={styles.line}>;</span>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Code;
--- src/container/Toolbar/index.tsx
+++ src/container/Toolbar/index.tsx
@@ -5,16 +5,20 @@ import Content from '../Content';
 import Fill from '../Fill';
 import Font from '../Font';
 import Code from '../Code';
+import MyScrollBox from '@components/Toolbar/MyScrollBox';
 
 class Toolbar extends React.Component {
   render() {
+    const height = document.body.clientHeight;
     return (
       <div className={styles.toolbar}>
-        <Layout />
-        <Fill />
-        <Font />
-        <Content />
-        <Code />
+        <MyScrollBox maxHeight={height}>
+          <Layout />
+          <Fill />
+          <Font />
+          <Content />
+          <Code />
+        </MyScrollBox>
       </div>
     );
   }

087f250b55e3574925c77d2fc698e6e25d846dfb
彭道宽 <pengdaokuan@cvte.com>
2020-11-01 16:23:17 +0800

    [feature][what]新增 store 路径

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -4,6 +4,7 @@ module.exports = {
   resolve: {
     alias: {
       '@model': path.resolve(__dirname, '../src/model/'),
+      '@store': path.resolve(__dirname, '../src/store/'),
       '@common': path.resolve(__dirname, '../src/common/'),
       '@components': path.resolve(__dirname, '../src/components/')
     },

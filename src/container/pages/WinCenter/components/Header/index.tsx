7c7214b09fe121aed0b9e5f3e6e400c804f90dce
彭道宽 <pengdaokuan@cvte.com>
2020-11-01 16:24:15 +0800

    [feature][what]完成机型model及引入

--- src/container/pages/WinCenter/components/Header/index.tsx
+++ src/container/pages/WinCenter/components/Header/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import styles from './index.less';
-import { Switch } from 'antd';
+import { Switch, Menu, Dropdown, Button } from 'antd';
+import { PHONE_MAPS } from '@common/constant';
+import globalModel from '@store/global';
 
 function Header() {
+  const { selectPhone, changeSelectPhone } = globalModel.usePhoneModel();
+
+  function renderMenu() {
+    return (
+      <Menu
+        onClick={({ key }) => {
+          changeSelectPhone(key);
+        }}
+      >
+        {Object.keys(PHONE_MAPS).map((rect: string, index: number) => {
+          return <Menu.Item key={rect}>{rect}</Menu.Item>;
+        })}
+      </Menu>
+    );
+  }
   return (
     <div className={styles.header}>
       <div className={styles.view} onClick={() => {}}>
         开启全屏
-        <Switch size="small" checked={true} className={styles.switch} />
+        <Switch size="small" checked={false} className={styles.marginLeft} />
+      </div>
+      <div className={styles.view} onClick={() => {}}>
+        机型尺寸
+        <Dropdown overlay={renderMenu} placement="bottomLeft" className={styles.marginLeft} trigger={['click']}>
+          <Button className={styles.phoneBtn} onClick={e => e.preventDefault()}>
+            {selectPhone}
+          </Button>
+        </Dropdown>
       </div>
     </div>
   );
